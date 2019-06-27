b3f832532957bf6756d5543614f8e08cfe3169a9
Jee Ken Chung CESM2016 <jee.chung.2016@uni.strath.ac.uk>
2019-06-27 13:26:49 +0100

    Corrected import statements

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import AppBar from "./MenuBar";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -21,18 +20,14 @@ export default class ProfilePage extends React.Component {
 
     render() {
         return (
-            <AppBar />
+            <Paper>
+                <Typography variant="h5" component="h3">
+                    This is a sheet of paper.
+                </Typography>
+                <Typography component="p">
+                    Paper can be used to build surface or other elements for your application.
+                </Typography>
+            </Paper>
         )
-        // const classes = useStyles();
-        // return (
-        //     <Paper className={classes.root}>
-        //         <Typography variant="h5" component="h3">
-        //             This is a sheet of paper.
-        //         </Typography>
-        //         <Typography component="p">
-        //             Paper can be used to build surface or other elements for your application.
-        //         </Typography>
-        //     </Paper>
-        // )
     }
 }
