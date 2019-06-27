42449eb62fffa0e23260cf652eb82b9edabf1cda
SimonCadge <SimonHCadge@gmail.com>
2019-06-27 13:29:09 +0100

    Cleanup

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Home from '../home/Home';
-import MenuBar from './MenuBar.js';
-import ProfilePage from './ProfilePage.js';
+import MenuBar from './MenuBar';
+import ProfilePage from '../ProfilePage/ProfilePage';
 
 function App() {
   return (
     <Router>
       <div>
-        <Header />
+        <MenuBar />
 
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/profilePage" component={ProfilePageFunction} />
+        <Route path="/profilePage" component={ProfilePage} />
       </div>
     </Router>
   );
