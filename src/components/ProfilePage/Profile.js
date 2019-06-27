b9715f7f7f7b63562f51b1ca05fc9cd1d7f96ec4
Jee Ken Chung CESM2016 <jee.chung.2016@uni.strath.ac.uk>
2019-06-27 16:11:26 +0100

    Cleaned up profile page

--- src/components/ProfilePage/Profile.js
+++ src/components/ProfilePage/Profile.js
@@ -19,7 +19,7 @@ export default class Profile extends React.Component {
             secondName: this.props.secondName,
             role: this.props.role,
             email: this.props.email,
-            description: this.props.description
+            description: "fasdf"
         }
     }
 
@@ -27,51 +27,36 @@ export default class Profile extends React.Component {
         return (
             <div className="container">
                 <div className="span3 well">
-
-                            <div className="modal-content">
-                                <Card>
-                                    <CardActionArea>
-                                        <CardMedia
-                                            image = ".././assets/images/genericProfilePicture.jpg"
-                                        />
-                                        <CardContent>
-                                            <h3 className="media-heading">{this.state.firstName + ' ' + this.state.secondName}<small>{this.state.role}</small></h3>
-                                            <Typography variant="body2" color="textSecondary" component="p">
-                                                { this.state.email }
-                                            </Typography>
-                                            <Typography variant="body2" color="textSecondary" component="p">
-                                                { this.state.description }
-                                            </Typography>
-                                        </CardContent>
-                                    </CardActionArea>
-                                    <CardActions>
-                                        <Button size="small" color="primary">
-                                            Share
-                                        </Button>
-                                        <Button size="small" color="primary">
-                                            Learn More
-                                        </Button>
-                                    </CardActions>
-                                    <CardActions>
-                                        <button onClick={this.onPlay}>Click me </button>
-                                    </CardActions>
-                                </Card>
-                                <div className="modal-body">
-                                    <center>
-                                        <img
-                                            src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbezqZpEuwGSvitKy3wrwnth5kysKdRqBW54cAszm_wiutku3R"
-                                            name="aboutme" width="140" height="140" border="0" className="img-circle"/>
-                                        <span><strong>Role: </strong></span>
-                                        <span>{this.state.role}</span>
-                                    </center>
-                                    <center>
-                                        <p className="text-left"><strong>Description: </strong>
-                                            { this.state.description }
-                                        </p>
-                                    </center>
-                                </div>
-                            </div>
-                        
+                    <div className="modal-content">
+                        <div className="modal-body">
+                            <center>
+                                <img
+                                    src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRbezqZpEuwGSvitKy3wrwnth5kysKdRqBW54cAszm_wiutku3R"
+                                    name="aboutme" width="140" height="140" border="0" className="img-circle"/>
+                            </center>
+                        </div>
+                        <Card>
+                            <CardActionArea>
+                                <CardMedia
+                                    image = ".././assets/images/genericProfilePicture.jpg"
+                                />
+                                <CardContent>
+                                    <h3 className="media-heading">{this.state.firstName + ' ' + this.state.secondName}<small>{this.state.role}</small></h3>
+                                    <Typography variant="body1" color="textSecondary" component="p">
+                                        { this.state.email }
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary" component="p">
+                                        { this.state.description }
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Pronounce name
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </div>
                 </div>
             </div>
         )
