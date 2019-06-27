8e090540bb71021042537eebf68e513d9cdfb8dc
SimonCadge <SimonHCadge@gmail.com>
2019-06-27 14:41:46 +0100

    State stuff

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,18 +5,36 @@ import Home from '../home/Home';
 import MenuBar from './MenuBar';
 import ProfilePage from '../ProfilePage/ProfilePage';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <MenuBar />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/profilePage" component={ProfilePage} />
-      </div>
-    </Router>
-  );
+    this.state = {
+      name: ''
+    }
+  }
+
+  isLoggedIn() {
+    if (this.state.name === '') {
+      return <Home />;
+    } else {
+      return <ProfilePage name={this.state.name}/>;
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <MenuBar />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/profilePage" render={this.isLoggedIn.bind(this)} /> 
+        </div>
+      </Router>
+    );
+  }
 }
 
 function About() {
