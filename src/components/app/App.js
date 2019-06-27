b777332382fb5dbf450af6ca00e1f6b4fe1ff701
Jee Ken Chung CESM2016 <jee.chung.2016@uni.strath.ac.uk>
2019-06-27 13:13:36 +0100

    Editing the profile page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 import Home from '../home/Home';
 import MenuBar from './MenuBar.js';
+import ProfilePage from './ProfilePage.js';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
 
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/profilePage" component={ProfilePageFunction} />
       </div>
     </Router>
   );
@@ -21,11 +23,18 @@ function About() {
   return <h2>About</h2>;
 }
 
+// function ProfilePageFunction() {
+//     return <ProfilePage />;
+// }
+
 function Header() {
   return (
       <MenuBar />
-
   );
+}
+
+function ProfilePageFunction() {
+    return <ProfilePage/>;
 }
 
 export default App;
