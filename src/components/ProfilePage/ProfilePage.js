ce3b53252378f3171bda1ca5f31b0288a2ed740c
Kristina <kidiki97@gmail.com>
2019-06-27 14:53:49 +0100

    audio button simple added

--- src/components/ProfilePage/ProfilePage.js
+++ src/components/ProfilePage/ProfilePage.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 
+import soundFile from './kk.mp3';
+
 const useStyles = makeStyles(theme => ({
     root: {
         padding: theme.spacing(3, 2),
@@ -16,18 +18,28 @@ export default class ProfilePage extends React.Component {
         this.state = {
 
         }
+        this.onPlay = this.onPlay.bind(this);
+        this.sound = new Audio(soundFile);
+          }
+
+        onPlay(){
+          this.sound.play();
     }
+
 
     render() {
         return (
-            <Paper>
-                <Typography variant="h5" component="h3">
-                    This is a sheet of paper.
-                </Typography>
-                <Typography component="p">
-                    Paper can be used to build surface or other elements for your application.
-                </Typography>
-            </Paper>
+
+            // <Paper>
+            //     <Typography variant="h5" component="h3">
+            //         This is a sheet of paper.
+            //     </Typography>
+            //     <Typography component="p">
+            //         Paper can be used to build surface or other elements for your application.
+            //     </Typography>
+            // </Paper>
+
+            <button onClick={this.onPlay}>Click me </button>
         )
     }
 }
