7f859ab2d8066803c4409433f9bbb965a11fac54
Gregor Kerr <gregorkerr@Gregors-MacBook-Pro.local>
2019-06-27 12:47:47 +0100

    Folder dir sorted

---
+++ src/components/Login/Login.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import AppBar from "./MenuBar";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(3, 2),
+    },
+}));
+
+export default class ProfilePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <AppBar />
+        )
+        // const classes = useStyles();
+        // return (
+        //     <Paper className={classes.root}>
+        //         <Typography variant="h5" component="h3">
+        //             This is a sheet of paper.
+        //         </Typography>
+        //         <Typography component="p">
+        //             Paper can be used to build surface or other elements for your application.
+        //         </Typography>
+        //     </Paper>
+        // )
+    }
+}
